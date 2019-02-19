5cab1e7daa71655fb64e39cbf934cdfb70e0bd00
imtiazraqib <imtiazud@ualberta.ca>
2019-02-18 23:57:58 -0700

    Initializing the android studio project

---
+++ app/src/main/java/vl/team07/com/virtuallibrary/MainActivity.java
@@ -0,0 +1,23 @@
+/*
+ * Class Name
+ *
+ * February 18, 2019
+ *
+ * Copyright @ 2019 Team 07, CMPUT 301, University of Alberta - All Rights Reserved.
+ * You may use, distribute, or modify this code under terms and conditions of the Code of Student Behaviour at the University of Alberta.
+ * You can find a copy of the license in the github wiki for this project.
+ */
+
+package vl.team07.com.virtuallibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
