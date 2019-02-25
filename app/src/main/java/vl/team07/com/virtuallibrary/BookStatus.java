2165cd6648b8d534cd64b5d5e75e6a74875515cd
keveren <kevin6@ualberta.ca>
2019-02-25 01:24:08 -0700

    Search Testing added
    Also took a couple of Matthew's classes like BookList to help with testing.

---
+++ app/src/main/java/vl/team07/com/virtuallibrary/BookStatus.java
@@ -0,0 +1,20 @@
+/*
+ * Class Name
+ *
+ * Date of Initiation
+ *
+ * Copyright @ 2019 Team 07, CMPUT 301, University of Alberta - All Rights Reserved.
+ * You may use, distribute, or modify this code under terms and conditions of the Code of Student Behaviour at the University of Alberta.
+ * You can find a copy of the license in the github wiki for this project.
+ */
+
+package vl.team07.com.virtuallibrary;
+
+/**
+ * Created by MTX on 2019-02-24.
+ */
+
+public enum BookStatus {
+    AVAILABLE,
+    BORROWED
+}
