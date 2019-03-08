47fe52ee48ba127c2c5abf17bd7d3e3cb4535985
tianxin3 <tianxin3@ualberta.ca>
2019-03-08 00:22:43 -0700

    Tab ViewPager Adapter Update

---
+++ app/src/main/java/vl/team07/com/virtuallibrary/TabViewPagerAdapter.java
@@ -0,0 +1,48 @@
+/*
+ * Class Name
+ *
+ * Date of Initiation
+ *
+ * Copyright @ 2019 Team 07, CMPUT 301, University of Alberta - All Rights Reserved.
+ * You may use, distribute, or modify this code under terms and conditions of the Code of Student Behaviour at the University of Alberta.
+ * You can find a copy of the license in the github wiki for this project.
+ */
+
+package vl.team07.com.virtuallibrary;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class TabViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public TabViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
