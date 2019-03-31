8a2b57b0b66ce899eecc90edf10e80b768d6d338
keveren <kevin6@ualberta.ca>
2019-03-31 00:34:57 -0600

    Search function working

--- app/src/main/java/vl/team07/com/virtuallibrary/SearchFragment.java
+++ app/src/main/java/vl/team07/com/virtuallibrary/SearchFragment.java
@@ -15,7 +15,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -24,9 +28,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityManager.TouchExplorationStateChangeListener;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SearchFragment extends android.support.v4.app.Fragment {
+
+    RecyclerView recyclerView;
+    public ArrayList<Book> allBookList;
+    public BookRecyclerViewAdapter mAdapter;
+    EditText search;
 
 
     public SearchFragment() {
@@ -38,6 +51,8 @@ public class SearchFragment extends android.support.v4.app.Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
         }
+
+
     }
 
     @Override
@@ -45,34 +60,81 @@ public class SearchFragment extends android.support.v4.app.Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View SearchView = inflater.inflate(R.layout.fragment_search, container, false);
-        getActivity().setTitle("Search");
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        //getActivity().setTitle("Search");
 
-        return SearchView;
-    }
+        recyclerView = (RecyclerView) view.findViewById(R.id.SearchRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        allBookList = new ArrayList<>();
+        mAdapter = new BookRecyclerViewAdapter(getContext(), allBookList);
+        recyclerView.setAdapter(mAdapter);
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
-        inflater.inflate(R.menu.search, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        SearchView searchView = new SearchView(((MainActivity) getActivity()).getSupportActionBar().getThemedContext());
-        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW | MenuItem.SHOW_AS_ACTION_IF_ROOM); item.setActionView(searchView);
-//        MenuItemCompat.setShowAsAction(item, MenuItemCompat.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW | MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
-//        MenuItemCompat.setActionView(item, sv);
+        tempList();
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        search = view.findViewById(R.id.searchText);
+        search.addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onQueryTextSubmit(String s) {
-                Log.d("TEST....", "Search Query Submitted");
-                return false;
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
             }
 
             @Override
-            public boolean onQueryTextChange(String s) {
-                Log.d("TEST 2....", "tap");
-                return false;
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
             }
         });
+        /**
+        RecyclerView mRecycler = view.findViewById(R.id.SearchRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        //tempList();
+        editTextSearch = (EditText) view.findViewById(R.id.searchText);
+        recyclerView.setHasFixedSize(true);
+        mAdapter = new BookRecyclerViewAdapter(getContext(),allBookList);
+        recyclerView.setAdapter(mAdapter);**/
+        return view;
     }
+
+    private void filter(String text){
+        ArrayList<Book> filteredList = new ArrayList<>();
+
+        for (Book books : allBookList){
+            if (books.getTitle().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(books);
+            }
+        }
+        mAdapter.filterList(filteredList);
+    }
+
+    public void tempList(){
+
+        User user = new User("Test user", "Test name", 0, "Test email", 0, "Canada", 0, "");
+
+        Book testBook = new Book("First Book", "First Author", 1234567890, user, BookStatus.AVAILABLE, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Second Book", "Second Author", 1234567890, user, BookStatus.BORROWED, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Third Book", "Third Author", 1234567890, user, BookStatus.AVAILABLE, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Forth Book", "Forth Author", 1234567890, user, BookStatus.BORROWED, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Fifth Book", "Fifth Author", 1234567890, user, BookStatus.AVAILABLE, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Sixth Book", "Sixth Author", 1234567890, user, BookStatus.BORROWED, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Seventh Book", "Seventh Author", 1234567890, user, BookStatus.AVAILABLE, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Eighth Book", "Eighth Author", 1234567890, user, BookStatus.BORROWED, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Ninth Book", "Ninth Author", 1234567890, user, BookStatus.AVAILABLE, "Description","SSN",null);
+        allBookList.add(testBook);
+        testBook = new Book("Tenth Book", "Tenth Author", 1234567890, user, BookStatus.BORROWED, "Description","SSN",null);
+        allBookList.add(testBook);
+    }
+
 }
 
